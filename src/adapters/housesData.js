6a5f57c660c8420fb6d2e3ba7b468116f3bd2900
Damian Sire <damiansire@hotmail.com>
2021-11-23 00:06:45 -0300

    Agrego like, dislike y muestra selectiva

--- src/adapters/housesData.js
+++ src/adapters/housesData.js
@@ -13,7 +13,7 @@ export async function getTodayHouseData() {
 export async function getPendingMatchHouseData() {
   let result;
   try {
-    const response = await fetch("http://localhost:3001/news");
+    const response = await fetch("http://localhost:3001/houses/news");
     result = await response.json();
   } catch (error) {
     console.log(error);
